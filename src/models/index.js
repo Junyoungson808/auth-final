6fb9c739b491717eebe7cb8d3cbc5b8cad9adb00
Junyoung Son <junyoungson808@gmail.com>
2022-10-01 06:57:44 +0000

    server running

--- src/models/index.js
+++ src/models/index.js
@@ -1,10 +1,8 @@
 'use strict';
 
 require('dotenv').config();
+
 const { Sequelize, DataTypes } = require('sequelize');
-// const iceCreamModel = require('./icecream/icecream');
-// const foodModel = require('./food/model.js');
-// const userModel = require('./users');
 const Collection = require('./model-interface.js');
 
 const DATABASE_URL = process.env.DATABASE_URL || 'sqlite:memory';
@@ -19,7 +17,8 @@ const flavors = require('./flavors/flavors')(sequelize, DataTypes);
 user.hasMany(icecream);
 icecream.belongsTo(user);
 icecream.hasOne(flavors);
-flavors.belongsToMany(icecream);
+icecream.belongsTo(flavors);
+flavors.hasMany(icecream);
 
 //EXPORTS
 module.exports = {
